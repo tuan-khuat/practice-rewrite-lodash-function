e0d68eaa2e4ba01632a0d977bb258d3877e80886
tuan-khuat <tuan.khuat@corize.co.jp>
2022-11-08 21:20:22 +0900

    chunk function

---
+++ chunk.ts
@@ -0,0 +1,9 @@
+// _.xorBy([2.1, 1.2], [2.3, 3.4], Math.floor);
+// // => [1.2, 3.4]
+
+// // The `_.property` iteratee shorthand.
+// _.xorBy([{ x: 1 }], [{ x: 2 }, { x: 1 }], 'x');
+// // => [{ 'x': 2 }]
+
+
+
